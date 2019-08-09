9cd9d77e34c82868ca84a6a440b784fc01f0e55e
Pavel Karkh <pavelkarkh@gmail.com>
2019-08-09 19:28:46 +0500

    find bug in parcel

--- packages/mono-web/src/client/Home.js
+++ packages/mono-web/src/client/Home.js
@@ -1,11 +1,13 @@
 import React, {Fragment} from 'react'
 import Button from 'mono-components/Button'
 import Header from './components/Header'
+// import ClassTest from './components/ClassTest'
 
 export default () => (
   <Fragment>
     <Header />
     <div>Body text</div>
     <Button green>Click ME</Button>
+    {/* <ClassTest /> */}
   </Fragment>
 )
